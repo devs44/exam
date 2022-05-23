a305bea2435fb8ebd49c85132738b04860748c65
unknown <devs44sunuwar@gmail.com>
2022-05-23 14:43:44 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,138 @@
+// console.log("Hello Devi");
+// var fullName = "Devi Sunuwar";
+// var courseName = "React Js Bootcamp";
+// var isLoggedIn = false;
+// var loggedCount = 34;
+// // console.log(loggedCount);
+// var paymentMode;
+//
+// paymentMode = "Credit Card";
+//
+// console.log(paymentMode);
+
+// const uid = "abc123";
+// var fullName = "Devi Sunuwar";
+// var email = 'devs4sunuwar@gmail.com';
+// var password = "123456";
+// var confirmpassword = "123456";
+// var courseCount  = 0;
+// var isLoggedInFromGoggle = false;
+// // fullname = prompt("Enter your name");
+// console.log(fullName);
+// console.log(email);
+// console.log(uid);
+// console.log(`
+//   With unique ID: ${uid}
+//   User is: ${email}
+//   and her email is: ${email}
+//   and uses the password: ${password}
+// `);
+// var num1=7;
+// var num2=6;
+// console.log(num1*num2);
+// var answer = num1 >num2
+// console.log(answer);
+
+//
+// var sellingPrice = 600
+// var listPrice = 650
+//
+// var discountpercent = (listPrice-sellingPrice)/listPrice;
+// console.log(discountpercent);
+//
+// displaydiscountpercent = Math.round(discountpercent);
+// console.log(displaydiscountpercent);
+
+// var temperature;
+// temperature = 5;
+// if (temperature>10){
+//     console.log("Its very cold outside");
+// } else{
+//   console.log("Its very hot");
+// }
+
+// var isLoggedIn = true;
+// var isEmailVerified = true;
+// var cardInfo = false;
+// // if (isLoggedIn){
+// //   console.log("logged in success");
+// //   if (isEmailVerified){
+// //     console.log("Email is verified");
+// //     if (cardInfo){
+// //       console.log("Cardinfo is verified");
+// //     }
+// //   }
+// // }
+// if(isLoggedIn && isEmailVerified && cardInfo){
+//   console.log("You can make purchase")
+// }else{
+//   console.log("you cannot make purchase")
+// }
+
+// #ternary operator
+// var authenticated = false
+// authenticated ? console.log("SignOut Button") : console.log("LOGIN");
+// true part, false part
+
+// var user = "abc";
+// switch (user) {
+//   case "admin":
+//     console.log("You get full access");
+//     break;
+//   case "subadmin":
+//       console.log("You get half access");
+//       break;
+//   case "testprep":
+//         console.log("You get adhi half access");
+//         break;
+//   case "user":
+//           console.log("You get no access");
+//           break;
+//   default:
+//     console.log("Trial user");
+// }
+//
+// function sayHello(name) {
+//   console.log(`Hello there, ${name}.How are ayou?`);
+//   console.log(` ${name}`)
+//   console.log(`${name}, ${name}`)
+// }
+// sayHello("Devi");
+//
+// function greetings(){
+//   return "Helllo Nepal"
+// }
+//
+// var hello = greetings()
+// console.log(hello);
+// console.log(greetings());
+
+// function getUserRole(name, role){
+//   switch (role) {
+//     case "admin":
+//       return `${name} is admin with all access`
+//       break;
+//     case "subamdin":
+//       return `${name} is admin with half access`
+//       break;
+//     case "noadmin":
+//       return `${name} is admin with no access`
+//       break;
+//     default:
+//       return `${name}`
+//   }
+// }
+//
+//
+// console.log(getUserRole("devi", "admin" ));
+//
+// function tipper(a){
+//   var bill = parseInt(a);
+//   console.log(bill +5);
+// }
+// tipper("5")
+bigTipper("200")
+function bigTipper(a){
+  var bill = parseInt(a);
+  console.log(bill=15);
+}
